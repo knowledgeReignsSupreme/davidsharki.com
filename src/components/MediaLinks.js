b7ec9c77c56f128dc7f98a233728ab2efcd0b490
David <david17895@gmail.com>
2021-06-18 14:08:50 +0300

    media links animation

--- src/components/MediaLinks.js
+++ src/components/MediaLinks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaAt, FaFilePdf, FaLinkedin } from 'react-icons/fa';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export default function MediaLinks() {
   return (
@@ -22,6 +22,16 @@ export default function MediaLinks() {
   );
 }
 
+const slideFromLeft = keyframes`
+from {
+  transform: translateX(-100px);
+
+}
+  to {
+    transform: translateY(0);
+  }
+`;
+
 const StyledMediaLinks = styled.div`
   position: fixed;
   left: 0;
@@ -33,9 +43,10 @@ const StyledMediaLinks = styled.div`
   background: ${(props) => props.theme.fontColor};
   padding: 0.3rem 0.6rem;
   border-radius: 10px;
+  animation: ${slideFromLeft} 8s ease-out;
 
   a {
-    font-size: 1.5rem;
+    font-size: calc(1rem + 0.5vw);
     color: ${(props) => props.theme.main};
     display: block;
 
