9fe0506dadcfb34580e274e4125b8f964ce380ff
David <david17895@gmail.com>
2021-06-21 11:04:02 +0300

    ga

--- src/Analytics.js
+++ src/Analytics.js
@@ -1,6 +1,7 @@
 import ReactGA from 'react-ga';
 
-export const gaInit = ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+export const gaInit = () =>
+  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
 
 export const reportEvent = (category, event) => {
   ReactGA.event({
