47611c4448b8360cb564fb26165238004e44a514
David <david17895@gmail.com>
2021-06-29 09:55:16 +0300

    deleted clgs

--- src/Analytics.js
+++ src/Analytics.js
@@ -1,13 +1,11 @@
 import ReactGA from 'react-ga';
 
 export const gaInit = () => {
-  console.log(process.env.REACT_APP_GA_TRACKING_ID);
   ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID, []);
   ReactGA.pageview(window.location.pathname);
 };
 
 export const reportEvent = (category, event) => {
-  console.log('event');
   ReactGA.event({
     category,
     event,
